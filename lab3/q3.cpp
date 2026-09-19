8a04123c6b960b79376f0a119d05c335e8063a8b
chauhansatyam024-debug <chauhansatyam024@gmail.com>
2026-09-20 05:18:09 +0530

    update

--- lab3/q3.cpp
+++ lab3/q3.cpp
@@ -1,3 +1,30 @@
 //
 // Created by satyamchauhan on 20/09/26.
 //
+#include<iostream>
+#include<algorithm>
+void print(int arr[],int n ) {
+    for (int i = 0 ; i<n ; i++) {
+        std::cout<<arr[i]<<" ";
+    }
+    std::cout<<std::endl;
+}
+void ascend(int arr[] , int n ) {
+    for (int i = 1 ; i<n ; i++) {
+        int curr = arr[i];
+        int j = i-1;
+        while (j>=0 && arr[j]>curr) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = curr;
+        print(arr,n);
+    }
+}
+int main() {
+    int arr[] = {42,23,1,4,5,131,4,44};
+    int n = sizeof arr / sizeof arr[0];
+    ascend(arr,n);
+
+    return 0;
+}
